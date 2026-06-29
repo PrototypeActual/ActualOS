0df104b51fa7faadd515047f8400d459aaf1ce90
Cristian Torres <18661121+PrototypeActual@users.noreply.github.com>
2026-06-29 14:49:24 -0400

    Bringing in changes from main project

--- build_files_desktop/build_desktop_stable.sh
+++ build_files_desktop/build_desktop_stable.sh
@@ -2,16 +2,15 @@
 
 set -ouex pipefail
 
+# Copy the contents of system_files/ of the git repo to /
+cp -avf "/ctx/system_files"/. /
+
 ### Install packages
 
-## Packages can be installed from any enabled yum repo on the image.
-## RPMfusion repos are available by default in ublue main images
-## List of rpmfusion packages can be found here:
-## https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/42/x86_64/repoview/index.html&protocol=https&redirect=1
-
-## VSCode preinstall tasks
-# rpm --import https://packages.microsoft.com/keys/microsoft.asc
-# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install programs with DNF
 # dnf5 -y install code python3-tkinter
--- build_files_steamdeck/build_steamdeck_stable.sh
+++ build_files_steamdeck/build_steamdeck_stable.sh
@@ -2,12 +2,15 @@
 
 set -ouex pipefail
 
+# Copy the contents of system_files/ of the git repo to /
+cp -avf "/ctx/system_files"/. /
+
 ### Install packages
 
-## Packages can be installed from any enabled yum repo on the image.
-## RPMfusion repos are available by default in ublue main images
-## List of rpmfusion packages can be found here:
-## https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/42/x86_64/repoview/index.html&protocol=https&redirect=1
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install programs with DNF
 dnf5 -y install python3-tkinter
